6ee102aad2fafc292e376812a1acbfde1f3154fd
Pía Ramirez <the.captain312@gmail.com>
2025-03-13 21:54:56 -0600

    Creamos estructuras nuevas y el archivo de conexión así como la bd

---
+++ config/conn.php
@@ -0,0 +1,24 @@
+<?php
+class conn {
+    protected $db;
+
+    public function __construct() {
+        $url = "localhost";
+        $bdname = "mesatickets";
+        $user = "root";
+        $pass = "";
+
+        try {
+            $this->db = new PDO("mysql:host=$url;dbname=$bdname;charset=utf8mb4", $user, $pass, [
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+            ]);
+        } catch (PDOException $e) {
+            die("Error en la conexión: " . $e->getMessage());
+        }
+    }
+    public function ruta(){
+        return "http://localhost/ticket/";
+    }
+}
+?>
